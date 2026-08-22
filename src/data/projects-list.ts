647e4fdb152a079c1a65d176f6b0f2b19ceac472
anemeth1998 <anemeth1998@gmail.com>
2026-08-22 16:03:49 -0400

    fix: restore full archive via photo/drawing split modules

--- src/data/projects-list.ts
+++ src/data/projects-list.ts
@@ -1,1 +1,5 @@
-REPLACE_ME
+import type { Project } from "./portfolio";
+import { PHOTO_PROJECTS } from "./projects-photo";
+import { DRAWING_PROJECTS } from "./projects-drawing";
+
+export const PROJECTS: Project[] = [...PHOTO_PROJECTS, ...DRAWING_PROJECTS];
